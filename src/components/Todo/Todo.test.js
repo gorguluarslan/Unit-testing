c66d8b55da298d7bdd0920c50978b52bbfa33954
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-01-26 20:15:09 +0300

    update: new test

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { type } from "@testing-library/user-event/dist/type";
 import Todo from "./index";
 
 describe("Todo testleri", () => {
@@ -15,5 +16,18 @@ describe("Todo testleri", () => {
     const items = screen.getAllByText(/Item/i);
 
     expect(items.length).toEqual(3);
+  });
+  test("Input ve button dokümanda bulunmalı", () => {
+    expect(button).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  });
+  test("inputa string girilip butona basınca listeye eklenmeli", () => {
+    //inputu doldur
+    const name = "Mehmet";
+    userEvent.type(input, name);
+    // butona tıkla
+    userEvent.click(button);
+    //assertion (bir şeylerin olmasını bekleme kısmı)
+    expect(screen.getByText(name).toBeInTheDocument);
   });
 });
